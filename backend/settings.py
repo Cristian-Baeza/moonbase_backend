7f017d7b2edf2a2814b4a5a327c1f9259d616526
Cristian-Baeza <cristianbaeza092@gmail.com>
2021-04-24 15:00:51 -0600

    switched to postgres db/migrated

--- backend/settings.py
+++ backend/settings.py
@@ -120,8 +120,8 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moonbase',
     }
 }
 
